5ec3e98eee954935c56e3592c261db77da582837
BransinO1 <corbinmeacham65@gmail.com>
2024-06-20 18:18:54 -0600

    Installed Swagger

--- server.js
+++ server.js
@@ -1,24 +1,28 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const mongodb = require('./data/database');
-
 const app = express();
+
 const port = process.env.PORT || 3000;
 
-// Middleware to parse JSON and urlencoded data
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// Route handling
+app.use(bodyParser.json());
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
+    );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    next();
+});
 app.use('/', require('./routes'));
 
-// Initialize the database and start the server
 mongodb.initDb((err) => {
     if (err) {
-        console.error('Failed to connect to the database:', err);
-        process.exit(1);
-    } else {
-        app.listen(port, () => {
-            console.log(`Database connected and server running on port ${port}`);
-        });
+        console.log(err);
+    } 
+    else {
+        app.listen(port, () => {console.log(`Database connected and server running on port ${port}`)});
     }
 });
+
